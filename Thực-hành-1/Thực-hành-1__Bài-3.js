fd9f52e7d49fdfd3e846e18e9c50089ac12bcfb8
Vũ Minh Anh <vuminhanh@MacBook-Pro-cua-Vu-2.local>
2022-06-08 11:57:14 +0700

    aa

---
+++ Thực-hành-1/Thực-hành-1__Bài-3.js
@@ -0,0 +1,21 @@
+function cal() {
+    let a = +document.getElementById('a').value;
+    let b = +document.getElementById('b').value;
+    let c = a % b;
+    console.log(c);
+    if ( c == 0){
+        alert(" a chia hết cho b");
+    }else{
+        alert("a không chia hết cho b");
+    }
+
+
+
+
+
+
+
+
+
+
+}
