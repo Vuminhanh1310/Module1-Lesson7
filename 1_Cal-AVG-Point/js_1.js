caf3138bdc3fb40ef9047bf823035d89cb030275
Vũ Minh Anh <vuminhanh@MacBook-Pro-cua-Vu-2.local>
2022-06-10 09:23:36 +0700

    update 10-06

--- 1_Cal-AVG-Point/js_1.js
+++ 1_Cal-AVG-Point/js_1.js
@@ -1,11 +1,9 @@
-function cal() {
+function cal_B1() {
     let a = +document.getElementById('a').value;
     let b = +document.getElementById('b').value;
     let c = +document.getElementById('c').value;
     let tb = (a + b + c)/3;
-    document.getElementById('result').innerHTML = " Kết quả: Điểm trung bình học tập là " + tb;
-
-
+    document.getElementById('result-B1').innerHTML = " Kết quả: Điểm trung bình học tập là " + tb;
 
 
 
